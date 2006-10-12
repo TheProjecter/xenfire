5de52277c3e8cf09b16a3bcc0084567139ccb7ce
jaya.singhvi <jaya.singhvi@17878767-a11a-0410-8a92-492b1bb2118e>
2006-10-12 08:40:24 +0000

    Xenfire SVN Repository

---
+++ source/bin/defaults/profile/Prefs.js
@@ -0,0 +1,51 @@
+# Mozilla User Preferences
+// This is a generated file!
+
+user_pref("general.useragent.vendor", "K-Meleon/0.8.1");
+user_pref("layout.xml.prettyprint", true);
+user_pref("network.dir.generate_html", true);
+user_pref("network.cookie.cookieBehavior", 0); // 0-Accept, 1-dontAcceptForeign, 2-dontUse, 3-p3p
+user_pref("keyword.enabled", false);
+user_pref("keyword.URL", "http://www.google.com/search?q=");
+user_pref("kmeleon.general.opengroup", "ID_OPEN_LINK|ID_OPEN_LINK_IN_BACKGROUND");
+user_pref("kmeleon.general.startHome", false);
+user_pref("kmeleon.display.disableResize", true);
+user_pref("kmeleon.display.width", 780);
+user_pref("kmeleon.display.height", 570);
+user_pref("kmeleon.display.maximized", false);
+user_pref("kmeleon.plugins.bmpmenu.load", false);
+user_pref("kmeleon.plugins.bookmarks.load", false);
+user_pref("kmeleon.plugins.external.load", false);
+user_pref("kmeleon.plugins.favorites.load", false);
+user_pref("kmeleon.plugins.fullscreen.load", true);
+user_pref("kmeleon.plugins.gestures.load", false);
+user_pref("kmeleon.plugins.history.load", false);
+user_pref("kmeleon.plugins.hotlist.load", false);
+user_pref("kmeleon.plugins.layers.load", false);
+user_pref("kmeleon.plugins.layers.rebar", false);
+user_pref("kmeleon.plugins.macros.load", true);
+user_pref("kmeleon.plugins.rebarmenu.load", false);
+user_pref("kmeleon.plugins.toolbars.load", false);
+user_pref("kmeleon.privacy.useragent0.name", "Default");
+user_pref("kmeleon.privacy.useragent0.string", "");
+user_pref("kmeleon.privacy.useragent1.name", "Netscape 6.1");
+user_pref("kmeleon.privacy.useragent1.string", "Mozilla/5.0 (Windows; U; WinNT4.0; en-US) Netscape6/6.1");
+user_pref("kmeleon.privacy.useragent2.name", "Netscape 4.76");
+user_pref("kmeleon.privacy.useragent2.string", "Mozilla/4.76 [en] (WinNT; U)");
+user_pref("kmeleon.privacy.useragent3.name", "MSIE 6.0");
+user_pref("kmeleon.privacy.useragent3.string", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)");
+user_pref("kmeleon.privacy.useragent4.name", "MSIE 5.5");
+user_pref("kmeleon.privacy.useragent4.string", "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)");
+user_pref("kmeleon.privacy.useragent5.name", "MSIE 4.0");
+user_pref("kmeleon.privacy.useragent5.string", "Mozilla/4.0 (compatible; MSIE 4.0; Windows NT)");
+user_pref("kmeleon.toolband.Bookmarks.visibility", false);
+user_pref("kmeleon.toolband.Layers.visibility", false);
+user_pref("kmeleon.toolband.Menu.visibility", false);
+user_pref("kmeleon.toolband.Menu Bar.visibility", false);
+user_pref("kmeleon.toolband.Throbber.visibility", false);
+user_pref("kmeleon.toolband.Tool Bar.visibility", false);
+user_pref("kmeleon.toolband.URL Bar buttons.visibility", false);
+user_pref("kmeleon.toolband.URL Bar.visibility", false);
+user_pref("dom.allow_scripts_to_close_windows", true);
+
+user_pref("kmeleon.general.offline",    true);
---
+++ source/disctree/incl/js/ocd_common.js
@@ -0,0 +1,115 @@
+
+
+/* ScreenShot() takes three arguments:
+   X is the width of the screenshot that is to be shown.
+   Y is the height of the screenshot to be shown.
+   source is the path to the screenshot.
+
+It also measures the resolution of the screen. If the screen is as large or larger
+than the screenshot to be shown, ScreenShot() pops up a window that big. If the
+screen is smaller than the screenshot, ScreenShot() pops up a window as large as
+the screen itself, and enables scroll bars.*/
+
+function ScreenShot(ShotX, ShotY, source){
+   // Width and Height of new window;
+   var ChildX, ChildY;
+   // User can resize/scroll new window?  Default no.
+   var resizability = "no";
+   var scrollable = "no";
+   // Available width and height in pixels -- doesn't count task bar.
+   var ScreenX = screen.availWidth;
+   var ScreenY = screen.availHeight;
+    var buffer = 20;
+    
+   // Scrollbars appear even if explicitly disabled unless the new window
+   // is at least 16 pixels wider than the screenshot in both dimensions.
+   // The scrollbars themselves are 16 px.  Could be a moz bug.
+   // If the window would bigger than or equal to the screensize after these
+   // pixels are added, instead use the screen dimensions minus 100 and make
+   // the window resizeable and scrollable.
+   if ((ShotX + buffer) >= ScreenX){
+      ChildX = ScreenX - 100;
+      resizability = "yes";
+      scrollable = "yes";
+   } else { ChildX = ShotX + buffer;  }
+   
+   if ((ShotY + buffer) >= ScreenY){
+      ChildY = ScreenY - 100;
+      resizability = "yes";
+      scrollable = "yes";
+   } else { ChildY = ShotY + buffer; }
+
+   // Assemble the list of features of the window.
+   features = "resizable="+resizability;
+   features += ",scrollbars="+scrollable;
+   features += ",width="+ChildX;
+   features += ",height="+ChildY;
+
+   source_line = '<img border="0" alt="image" src="'+source+'" /></a></div>';
+
+   // Open the window.
+   // window.open(source, "Thumbnail", features);
+   screen_window = window.open("", "Screen Capture Image", features);
+    screen_window.document.write("<html><title>Screen Capture Image</title>");
+    screen_window.document.write("<body bgcolor='gray' onblur=window.close()>");
+    screen_window.document.write('<div style="display:block" class="screen_block">');
+    screen_window.document.write('<div class="thumb_frame"><a href="javascript:window.close();">');  
+    screen_window.document.write(source_line);
+    screen_window.document.write("</div></body></html>");
+
+// If you just want to open an existing HTML page in the 
+// new window, you can replace win()'s coding above with:
+// window.open("page.html","","height=200,width=200,left=300,top=100");
+}
+
+/*PositionWindow() measures the resolution of the host screen, and uses that information to center the
+browser on the screen.  NOTE: check to see if it's possible to make K-Meleon do this itself, so that the
+screen doesn't shift noticeably on start.  This is called from the onLoad event.*/
+
+function PositionWindow(){
+   var Width, Height, xPos, yPos;
+   Width = screen.width;
+   Height = screen.height;
+   xPos = (Width - 640) / 2;
+   yPos = (Height - 480) / 2;
+   window.moveTo(xPos, yPos);
+}
+
+/* LangWindow() opens the language selection window */
+function PickLanguage()
+{
+window.open('../include/PickLanguage.html','mywindow','width=300,height=300,top=100,resizable=yes,scrollbars=yes');
+}
+
+// Jump to a given page
+function JumpToPage(Language){
+    var OldURL = String(self.opener.location);
+    var FileName = GetFileName(OldURL);
+    var PageURL = '';
+    PageURL = PageURL.concat('../',Language,'/',FileName);
+//    alert(PageURL);
+	self.opener.location = PageURL;
+	self.close();
+	}
+
+// Strips the FileName out of a URL
+function GetFileName (FullURL){
+    SubStrings = FullURL.split("/");
+    return SubStrings[SubStrings.length-1];
+    }
+
+/* InfoWindow */
+function OpenInfoWindow()
+{
+window.open('../include/InfoWindow.html','About','width=600,height=550,top=100,resizable=yes,scrollbars=yes');
+}
+
+function ExitProg()
+{
+// Ask for confirmation
+ var where_to= confirm("You are about to exit the CD browser. Click OK to exit.");
+ if (where_to== true)
+ {
+   window.close();
+ }
+}
